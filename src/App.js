8670d23109a1d36a6f620ee14d4c33c35c498304
cosmicsaurabh <saurabhyadavclg20@gmail.com>
2024-07-23 09:50:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QRCodeGen from './index.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGen/>
+    </div>
+  );
+}
+
+export default App;
